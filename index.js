422df11197d3a5310eb93ac1665515d8c8185666
Mary Kay_Kennedy <marykaykennedy@me.com>
2022-03-23 19:02:39 -0400

    made some changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Your Code Here
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
